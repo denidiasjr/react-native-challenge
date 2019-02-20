e8a46d4d6f58bae8735f354cdf0ac223c63f1594
Deni Junior <denidiasjr@gmail.com>
2019-02-20 00:06:03 -0300

    Testing react-navigation

--- mylist/App.js
+++ mylist/App.js
@@ -7,44 +7,36 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {
+  Text, 
+  View
+} from 'react-native';
+import {
+  createStackNavigator,
+  createAppContainer
+} from 'react-navigation';
+import HomeScreen from './src/screens/HomeScreen';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <AppContainer />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+const AppStackNavigator = createStackNavigator(
+  {
+    Home: HomeScreen
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: 'purple'
+      }
+    }
+  }
+);
+
+const AppContainer = createAppContainer(AppStackNavigator);
---
+++ mylist/src/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+    Text, 
+    View
+} from 'react-native';
+
+class HomeScreen extends React.Component {
+
+    render() {
+        return (
+            <View>
+                <Text>Home screen!</Text>
+            </View>
+        );
+    }
+}
+
+export default HomeScreen;
